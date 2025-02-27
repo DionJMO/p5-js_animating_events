86547201d376f6328553d1852f5793c02c4d4b1d
DionJMO <d.maringouambo@kroschke.com>
2025-02-27 15:57:23 +0100

    few changes

--- popover.js
+++ popover.js
@@ -1,12 +1,10 @@
-// const { title } = require("process");
-
 document.addEventListener("DOMContentLoaded", (event) => {
   const popoverTrigger = document.getElementById("popoverBtn");
   if (popoverTrigger) {
     const popover = new bootstrap.Popover(popoverTrigger, {
       trigger: "click",
-      content: "And here is some amazing content. It is very engaging. Right?",
-      title: "Popover Title",
+      content: data,
+      title: "Information",
     });
     // popoverTrigger.addEventListener("click", () => {
     //   popover.toggle();
